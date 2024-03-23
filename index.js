b57aabe000d46e10c50e91a1a53b217d26e7b6ce
Badar-BlueBird <abdulrehmanbadar191@gmail.com>
2024-03-23 06:33:12 +0500

    Changed Timing again

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 const port = 4000;
 
 
+
 async function readCsvAndExtractMlsNumbers(filePath, targetMlsNumber) {
     return new Promise((resolve, reject) => {
         fs.createReadStream(filePath)
@@ -59,49 +60,48 @@ app.get('/', (req, res) => {
         });
     
         //Xpaths for FMLS
-        const loginButton = `(//li[contains(@class, 'menu_cta')]/a[@title='Member Login to FMLS Products'])[2]`;
-        const userNameBtn = `(//input[contains(@class, 'form-control') and contains(@class, 'form__input-control')])[1]`
-        const passBtn = `(//input[contains(@class, 'form-control') and contains(@class, 'form__input-control')])[2]`
-        const submitBtn = `//button[@id='btn-login']`
-        const matrixBtn = `(//a[contains(@class, 'product-item_productItem__7tSq-')])[8]`
+        const loginButton = '#top-menu > li.menu_cta.menu-item.menu-item-type-custom.menu-item-object-custom.menu-item-872 > a';
+        const userNameBtn = 'input.form-control.form__input-control:nth-child(2)'
+        const passBtn = 'input.form-control.form__input-control:nth-child(1)'
+        const submitBtn = 'button#btn-login'
+        const matrixBtn = '#root > div.contentWrapper > div.body_body__1Ourk.undefined > div > div > div:nth-child(2) > div > div.responsiveness-columns_width66__1fgLg.layout-wrapper_layoutWrapper__2IT_n.column-layout_layoutWrapper__1p42U > div:nth-child(3) > div > div > div > a:nth-child(1)'
         // const residentialBtn = `(//li[contains(@style, 'white-space: nowrap; float: none; width: auto;')])[11]`
-        const selectAllCheckBoxes = `//a[text()='Select All']`
-        const unselectAllCheckBoxes = `//a[text()='Select None']`
+        const unselectAllCheckBoxes = '#S_MultiStatus_Select_None > a'
         //REMEBER : Check these boxes Active Under Contract = 3 , Pending = 4 ,  Closed = 8
-        const checkBoxes = `//input[@name='Fm23_Ctrl106_LB']`
+        const checkBoxes = 'input[name="Fm23_Ctrl106_LB"]'
         //REMEMBER : Textboxes are from range [2] - [9]  , i fill 4th , 5th and 9th with (0-1)
-        const textBoxes = `//input[@class='textbox']`
-        const result = `//a[@id='m_ucSearchButtons_m_lbSearch' and .//span[text()='Results']]`
-        const totalListings = `//span[@id='m_lblPagingSummary']//b[last()]`
-        const pageNumSelector = `//select[@name='m_ucDisplayPicker$m_ddlPageSize']`
-        const selectAllListings = `//a[@title='Check All']`
-        const exportButton = `//a[@id='m_lbExport']`
-        const exportCsv = `//*[@id="m_btnExport"]`
-        const goBack = `//*[@id="m_btnBack"]`
-        const MLSofListings = `//*[@id="wrapperTable"]/td[4]/span/a`
-        const getCurrListNum = `//td[@class="d162m26"]//span[@class="wrapped-field"]`
-        const clickSellerInfo = `(//*[@id="wrapperTable"]/tbody/tr/td/table/tbody/tr[6]/td[2]/span[2]/a)[2]`
-        const getSellerEmail = `//td[@class="d19m16"]//a`
-        const closeBtn = `//*[@id="m_aCloseWindow"]`
-        const nextPage = `(//a[contains(text(), 'Next')])[2]`
+        const textBoxes = 'input.textbox'
+        const result = '#m_ucSearchButtons_m_lbSearch'
+        const totalListings = 'span#m_lblPagingSummary b:last-child'
+        const pageNumSelector = 'select[name="m_ucDisplayPicker$m_ddlPageSize"]'
+        const selectAllListings = 'a[title="Check All"]'
+        const exportButton = 'a#m_lbExport'
+        const exportCsv = '*#m_btnExport'
+        const goBack = '*#m_btnBack'
+        const MLSofListings = '#wrapperTable > td.dU39611m8 > span > a'
+        const getCurrListNum = '#wrapperTable > tbody > tr > td > table > tbody > tr:nth-child(5) > td.d162m26 > span.wrapped-field'
+        const clickSellerInfo = '#wrapperTable > tbody > tr > td > table > tbody > tr:nth-child(6) > td.d168m13 > span.formula.field.d168m19 > a'
+        const getSellerEmail = 'td.d19m16 a'
+        const closeBtn = '#m_aCloseWindow'
+        const nextPage = '#m_upPaging > span > a:nth-child(11)'
         let totalNumofLists = 0;
         let currListNum = 0;
     
-        await page.waitForXPath(loginButton)
-        const [loginButtonXpath] = await page.$x(loginButton);
+        await page.waitForSelector(loginButton)
+        const loginButtonXpath = await page.$(loginButton);
         try {
             if (loginButtonXpath) {
                 await loginButtonXpath.click();
             }
     
-            await page.waitForXPath(userNameBtn)
-            await page.waitForXPath(passBtn)
-            await page.waitForXPath(submitBtn)
+            await page.waitForSelector(userNameBtn)
+            await page.waitForSelector(passBtn)
+            await page.waitForSelector(submitBtn)
     
     
-            const [userNameBtnXpath] = await page.$x(userNameBtn);
-            const [passBtnXpath] = await page.$x(passBtn);
-            const [submitBtnXpath] = await page.$x(submitBtn);
+            const userNameBtnXpath = await page.$(userNameBtn);
+            const passBtnXpath = await page.$(passBtn);
+            const submitBtnXpath = await page.$(submitBtn);
             try {
                 if (userNameBtnXpath) {
                     await userNameBtnXpath.type('MVDWING');
@@ -116,8 +116,8 @@ app.get('/', (req, res) => {
                 console.log(error);
             }
             await new Promise(resolve => setTimeout(resolve, 5000));
-            await page.waitForXPath(matrixBtn);
-            const [matrixBtnXpath] = await page.$x(matrixBtn);
+            await page.waitForSelector(matrixBtn);
+            const matrixBtnXpath = await page.$(matrixBtn);
             if (matrixBtnXpath) {
                 await page.evaluate((button) => {
                     button.setAttribute('target', '_self');
@@ -127,9 +127,8 @@ app.get('/', (req, res) => {
             await new Promise(resolve => setTimeout(resolve, 13000));
             await page.goto('https://matrix.fmlsd.mlsmatrix.com/Matrix/Search/Residential', { waitUntil: "domcontentloaded", });
     
-            await page.waitForXPath(selectAllCheckBoxes);
-            await page.waitForXPath(unselectAllCheckBoxes);
-            const [unselectCheckBoxesXpath] = await page.$x(unselectAllCheckBoxes);
+            await page.waitForSelector(unselectAllCheckBoxes);
+            const unselectCheckBoxesXpath = await page.$(unselectAllCheckBoxes);
             if (unselectCheckBoxesXpath) {
                 await new Promise(resolve => setTimeout(resolve, 4000));
                 await page.evaluate(() => {
@@ -139,12 +138,12 @@ app.get('/', (req, res) => {
                 await unselectCheckBoxesXpath.click();
             }
     
-            await page.waitForXPath(checkBoxes);
-            await page.waitForXPath(textBoxes);
-            const checkBoxesXpath = await page.$x(checkBoxes);
-            const textBoxesXpath = await page.$x(textBoxes);
-            await page.waitForXPath(result);
-            const [resultXpath] = await page.$x(result);
+            await page.waitForSelector(checkBoxes);
+            await page.waitForSelector(textBoxes);
+            const checkBoxesXpath = await page.$$(checkBoxes);
+            const textBoxesXpath = await page.$$(textBoxes);
+            await page.waitForSelector(result);
+            const resultXpath = await page.$(result);
             if (checkBoxesXpath) {
                 await new Promise(resolve => setTimeout(resolve, 3000));
                 const checkboxesArray = [checkBoxesXpath[2], checkBoxesXpath[3], checkBoxesXpath[7]];
@@ -169,17 +168,17 @@ app.get('/', (req, res) => {
                 await resultXpath.click();
             }
             await new Promise(resolve => setTimeout(resolve, 15000));
-            await page.waitForXPath(selectAllListings);
-            await page.waitForXPath(exportButton);
-            await page.waitForXPath(totalListings);
-            const [totalListingsXpath] = await page.$x(totalListings);
+            await page.waitForSelector(selectAllListings);
+            await page.waitForSelector(exportButton);
+            await page.waitForSelector(totalListings);
+            const totalListingsXpath = await page.$(totalListings);
     
             if (totalListingsXpath) {
                 let text1 = await page.evaluate(element => element.innerText, totalListingsXpath);
                 totalNumofLists = Number(text1);
             }
-            const [selectAllListingsXpath] = await page.$x(selectAllListings);
-            const [exportButtonXpath] = await page.$x(exportButton);
+            const selectAllListingsXpath = await page.$(selectAllListings);
+            const exportButtonXpath = await page.$(exportButton);
             if (pageNumSelector) {
                 await selectAllListingsXpath.click();
                 await new Promise(resolve => setTimeout(resolve, 2000));
@@ -189,41 +188,41 @@ app.get('/', (req, res) => {
                 await new Promise(resolve => setTimeout(resolve, 10000));
                 await page.select('select[id="m_ddExport"]', 'ud39051');
             }
-            await page.waitForXPath(exportCsv);
-            const [exportCsvXpath] = await page.$x(exportCsv);
+            await page.waitForSelector(exportCsv);
+            const exportCsvXpath = await page.$(exportCsv);
             await new Promise(resolve => setTimeout(resolve, 2000));
             if (exportCsvXpath) {
                 await exportCsvXpath.click()
                 console.log("Csv Downloaded")
             }
             await new Promise(resolve => setTimeout(resolve, 3000));
-            await page.waitForXPath(goBack);
-            const [goBackXpath] = await page.$x(goBack);
+            await page.waitForSelector(goBack);
+            const goBackXpath = await page.$(goBack);
             if (goBackXpath) {
                 await goBackXpath.click();
             }
             await new Promise(resolve => setTimeout(resolve, 10000));
     
             const filePath = path.join(__dirname, 'downloads', 'Agent Autopilot.csv');
-            await page.waitForXPath(MLSofListings);
-            const [MLSofListingsXpath] = await page.$x(MLSofListings);
+            await page.waitForSelector(MLSofListings);
+            const MLSofListingsXpath = await page.$(MLSofListings);
             if (MLSofListingsXpath) {
                 await MLSofListingsXpath.click();
             }
             for (let index = 0; index < totalNumofLists; index++) {
                 await new Promise(resolve => setTimeout(resolve, 2000));
-                await page.waitForXPath(getCurrListNum);
-                await page.waitForXPath(clickSellerInfo);
-                const getCurrListNumXpath = await page.$x(getCurrListNum);
-                const [clickSellerInfoXpath] = await page.$x(clickSellerInfo);
+                await page.waitForSelector(getCurrListNum);
+                await page.waitForSelector(clickSellerInfo);
+                const getCurrListNumXpath = await page.$(getCurrListNum);
+                const clickSellerInfoXpath = await page.$(clickSellerInfo);
                 if (getCurrListNumXpath) {
-                    let text = await page.evaluate(element => element.innerText, getCurrListNumXpath[0]);
+                    let text = await page.evaluate(element => element.innerText, getCurrListNumXpath);
                     currListNum = text;
                 } else {
                     console.log('Could not get List Number');
                 }
                 console.log("Current List : " + currListNum);
-                await new Promise(resolve => setTimeout(resolve, 4000));
+                await new Promise(resolve => setTimeout(resolve, 5000));
                 if (clickSellerInfoXpath) {
                     await clickSellerInfoXpath.click();
                 }
@@ -234,10 +233,10 @@ app.get('/', (req, res) => {
                 const lastPage = pages[pages.length - 1];
                 await new Promise(resolve => setTimeout(resolve, 5000));
     
-                await lastPage.waitForXPath(getSellerEmail);
-                const [getSellerEmailXpath] = await lastPage.$x(getSellerEmail);
-                await lastPage.waitForXPath(closeBtn);
-                const [closeBtnXpath] = await lastPage.$x(closeBtn);
+                await lastPage.waitForSelector(getSellerEmail);
+                const getSellerEmailXpath = await lastPage.$(getSellerEmail);
+                await lastPage.waitForSelector(closeBtn);
+                const closeBtnXpath = await lastPage.$(closeBtn);
     
                 let currSellerEmail = ''; 
                 currSellerEmail = await lastPage.evaluate((getSellerEmailXpath) => {
@@ -309,8 +308,8 @@ app.get('/', (req, res) => {
                     });
     
                 await new Promise(resolve => setTimeout(resolve, 4000));
-                await page.waitForXPath(nextPage);
-                const [nextPageXpath] = await page.$x(nextPage);
+                await page.waitForSelector(nextPage);
+                const nextPageXpath = await page.$(nextPage);
                 if (nextPageXpath) {
                     await nextPageXpath.click()
                 }
