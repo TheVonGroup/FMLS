5d0734100bd814f76ea570db9fea38771eb0bf39
Badar-BlueBird <abdulrehmanbadar191@gmail.com>
2024-03-23 06:43:11 +0500

    Changed Timing again and again

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ require('dotenv').config();
 const app = express();
 const port = 4000;
 
+const { join } = require('path');
+
+module.exports = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
+
 
 
 async function readCsvAndExtractMlsNumbers(filePath, targetMlsNumber) {
